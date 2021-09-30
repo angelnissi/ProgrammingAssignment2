b7c9bcd6dce6d4e6a0f99d34e941990e793c72e0
angelnissi <91533715+angelnissi@users.noreply.github.com>
2021-09-30 18:17:23 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+setwd('C:/Users/arnavsin/Documents/Coursera-R')
+##
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
 makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
   s <- NULL
   set <- function(y) {
